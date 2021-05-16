128c39928fd91feddf7314ef4365380be0707bd8
Andrei Zavatski <megaman9919@gmail.com>
2021-05-17 01:02:27 +0300

    Fix progress circle isn't smooth

--- osu.Game.Rulesets.Mvis/UI/Objects/BeatmapLogo.cs
+++ osu.Game.Rulesets.Mvis/UI/Objects/BeatmapLogo.cs
@@ -1,8 +1,9 @@
 ﻿using osu.Framework.Allocation;
 using osu.Framework.Graphics;
-using osu.Framework.Graphics.Effects;
-using osu.Framework.Graphics.UserInterface;
+using osu.Framework.Graphics.Containers;
+using osu.Framework.Utils;
 using osu.Game.Rulesets.Mvis.UI.Objects.Helpers;
+using osu.Game.Screens.Ranking.Expanded.Accuracy;
 using osuTK;
 using osuTK.Graphics;
 
@@ -10,19 +11,13 @@ namespace osu.Game.Rulesets.Mvis.UI.Objects
 {
     public class BeatmapLogo : CurrentBeatmapProvider
     {
-        private Color4 colour;
-
         public new Color4 Colour
         {
-            get => colour;
-            set
-            {
-                colour = value;
-                progress.Colour = value;
-            }
+            get => progress.Colour;
+            set => progress.Colour = value;
         }
 
-        private CircularProgress progress;
+        private Progress progress;
 
         [BackgroundDependencyLoader]
         private void load()
@@ -38,17 +33,13 @@ namespace osu.Game.Rulesets.Mvis.UI.Objects
                     Anchor = Anchor.Centre,
                     Origin = Anchor.Centre,
                 },
-                (progress = new CircularProgress
+                progress = new Progress
                 {
                     Anchor = Anchor.Centre,
                     Origin = Anchor.Centre,
                     RelativeSizeAxes = Axes.Both,
-                    InnerRadius = 0.02f,
-                }).WithEffect(new GlowEffect
-                {
-                    Strength = 5,
-                    PadExtent = true
-                }),
+                    InnerRadius = 0.03f,
+                }
             };
         }
 
@@ -58,6 +49,35 @@ namespace osu.Game.Rulesets.Mvis.UI.Objects
 
             var track = Beatmap.Value?.Track;
             progress.Current.Value = (track == null || track.Length == 0) ? 0 : (track.CurrentTime / track.Length);
+        }
+
+        private class Progress : SmoothCircularProgress
+        {
+            private static readonly Vector2 sigma = new Vector2(5);
+
+            private BufferedContainer bufferedContainer => (BufferedContainer)InternalChild;
+
+            public new Color4 Colour
+            {
+                get => bufferedContainer.Colour;
+                set
+                {
+                    bufferedContainer.Colour = value;
+                    bufferedContainer.EffectColour = bufferedContainer.Colour.MultiplyAlpha(4);
+                }
+            }
+
+            public Progress()
+            {
+                bufferedContainer.BlurSigma = sigma;
+                bufferedContainer.DrawOriginal = true;
+                bufferedContainer.EffectBlending = BlendingParameters.Additive;
+                bufferedContainer.Padding = new MarginPadding
+                {
+                    Horizontal = Blur.KernelSize(sigma.X),
+                    Vertical = Blur.KernelSize(sigma.Y),
+                };
+            }
         }
     }
 }
