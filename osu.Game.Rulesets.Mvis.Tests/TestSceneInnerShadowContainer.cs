79d00a928476b7b6ed434abca898e94cf08adaa9
Andrei Zavatski <megaman9919@gmail.com>
2021-05-27 03:23:30 +0300

    Implement InnerShadowContainer

---
+++ osu.Game.Rulesets.Mvis.Tests/TestSceneInnerShadowContainer.cs
@@ -0,0 +1,60 @@
+﻿using osu.Framework.Graphics;
+using osu.Framework.Graphics.Containers;
+using osu.Framework.Graphics.Shapes;
+using osu.Game.Graphics;
+using osu.Game.Graphics.Sprites;
+using osu.Game.Overlays;
+using osu.Game.Rulesets.Mvis.UI;
+using osu.Game.Tests.Visual;
+using osuTK;
+
+namespace osu.Game.Rulesets.Mvis.Tests
+{
+    public class TestSceneInnerShadowContainer : OsuTestScene
+    {
+        private readonly OverlayColourProvider colourProvider = new OverlayColourProvider(OverlayColourScheme.Pink);
+
+        private TestContainer testContainer;
+
+        public TestSceneInnerShadowContainer()
+        {
+            AddRange(new Drawable[]
+            {
+                new Box
+                {
+                    RelativeSizeAxes = Axes.Both,
+                    Colour = colourProvider.Background3
+                },
+                testContainer = new TestContainer()
+                {
+                    Anchor = Anchor.Centre,
+                    Origin = Anchor.Centre,
+                    Size = new Vector2(400)
+                }
+            });
+        }
+
+        protected override void LoadComplete()
+        {
+            base.LoadComplete();
+
+            AddSliderStep("Depth", 0, 100, 10, d => testContainer.Depth.Value = d);
+            AddSliderStep("Corner radius", 0, 100, 10, c => testContainer.CornerRadius.Value = c);
+        }
+
+        private class TestContainer : InnerShadowContainer
+        {
+            protected override Container<Drawable> CreateContent() => new Container
+            {
+                RelativeSizeAxes = Axes.Both,
+                Child = new OsuSpriteText
+                {
+                    Anchor = Anchor.Centre,
+                    Origin = Anchor.Centre,
+                    Text = "Test",
+                    Font = OsuFont.GetFont(size: 30)
+                }
+            };
+        }
+    }
+}
